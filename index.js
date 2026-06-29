220d744add3a749d205a66a6b6ed0e69e8aa1fcb
chunlik <chinly2005@gmail.com>
2026-06-29 20:44:40 +0800

    Pin JSTV root index.js ext to commit SHA raw URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
       "searchable": 1,
       "quickSearch": 1,
       "filterable": 1,
-      "ext": "https://raw.githubusercontent.com/chunlik/novel-creator/refs/heads/main/js/fdzys.js"
+      "ext": "https://raw.githubusercontent.com/chunlik/novel-creator/30ae6ebb5ba5227989bae223cedd1d8dc49b57dd/js/fdzys.js"
     }
   ]
 }
