13c614c8a2b1007e735d14207aea57547f410422
chunlik <chinly2005@gmail.com>
2026-06-29 21:07:13 +0800

    Add JSTV index config file

---
+++ index.config.js
@@ -0,0 +1,16 @@
+var index_config_default = {
+  sites: {
+    list: [],
+  },
+  pans: {
+    list: [],
+  },
+  danmu: {
+    urls: [],
+    autoPush: false,
+  },
+};
+
+module.exports = {
+  default: index_config_default,
+};
