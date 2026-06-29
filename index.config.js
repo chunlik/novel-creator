d2131327aaa32ce2d55453d2a68867af3156620b
chunlik <chinly2005@gmail.com>
2026-06-29 21:20:30 +0800

    Add visible demo T4 config item

--- index.config.js
+++ index.config.js
@@ -1,4 +1,35 @@
 var index_config_default = {
+  ali: {
+    token: "",
+    token280: "",
+  },
+  quark: {
+    cookie: "",
+  },
+  uc: {
+    cookie: "",
+    token: "",
+    ut: "",
+  },
+  y115: {
+    cookie: "",
+  },
+  muou: {
+    url: "",
+  },
+  wogg: {
+    url: "",
+  },
+  leijing: {
+    url: "",
+  },
+  tgsou: {
+    tgPic: false,
+    count: 0,
+    url: "",
+    channelUsername: "",
+  },
+  tgchannel: {},
   sites: {
     list: [],
   },
@@ -9,6 +40,19 @@ var index_config_default = {
     urls: [],
     autoPush: false,
   },
+  t4: {
+    list: [
+      {
+        name: "Demo T4",
+        address: "https://example.com",
+      },
+    ],
+  },
+  cms: {
+    list: [],
+  },
+  alist: [],
+  color: [],
 };
 
 module.exports = {
