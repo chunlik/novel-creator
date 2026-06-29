96fa9fe3a419a48827835f1479fb26d7024426eb
chunlik <chinly2005@gmail.com>
2026-06-29 20:40:13 +0800

    Add root-level JSTV index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "spider": "",
+  "sites": [
+    {
+      "name": "饭搭子影视",
+      "type": 3,
+      "api": "csp_fdzys",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1,
+      "ext": "https://raw.githubusercontent.com/chunlik/novel-creator/refs/heads/main/js/fdzys.js"
+    }
+  ]
+}
